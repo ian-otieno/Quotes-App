633ecd75420b2123cbefcbc9aeb6e77480009c1b
ian_otieno <ian.otieno@student.moringaschol.com>
2022-02-04 11:44:31 +0300

    add quote-details component

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuoteDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
